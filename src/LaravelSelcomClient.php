1fed10884f2fbaf8ff6512582f722a05e3b78857
omakei <omakei96@gmail.com>
2021-07-14 12:06:17 +0300

    hotfix code duplicate

--- src/LaravelSelcomClient.php
+++ src/LaravelSelcomClient.php
@@ -219,7 +219,6 @@ class LaravelSelcomClient
 
     public function sendRequest(string $type, string $url)
     {
-        $method = array_key_exists(strtolower($type), $this->lookupTable())?$this->lookupTable()[strtolower($type)] : null;
 
          $method = array_key_exists(strtolower($type), $this->lookupTable())?$this->lookupTable()[strtolower($type)] : null;
 
